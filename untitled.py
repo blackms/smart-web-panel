abc57b07c926f4bc39907b8939bbe2b5732980e5
Alessio Rocchi <rocchi.b.a@gmail.com>
2016-01-28 23:15:18 +0100

    little slep

--- untitled.py
+++ untitled.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, request
+from ArubaCloud.PyArubaAPI import CloudInterface
 
 app = Flask(__name__)
 
@@ -11,9 +12,13 @@ def login():
 def main():
     return render_template("main.html")
 
-@app.route('/login')
+@app.route('/login', methods=['POST'])
 def check_login():
-    pass
+    data = request.form
+    ci = CloudInterface(1)
+    ci.login(username=str(data['username']), password=str(data['password']), load=False)
+    ci.get_servers()
+    return Response(response="ok", status=200, mimetype="application/json")
 
 
 if __name__ == '__main__':
